b1fc6cc3d5e17872bb9fa3ae574dfbbfa3c69573
GabbardDesigns <45954270+GabbardDesigns@users.noreply.github.com>
2023-03-01 18:47:28 -0500

    Removed Asset Clearing for 0 Amount ASAs
    Asset removal for 0 holding ASAs removed.  Added link to Kinn's Bulk Opt Out as the removal of 0 ASAs posed issue with staked assets.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Migration.Algo
-An Algorand Wallet Migration Utility built in Python
+**Algorand Wallet Transfer Utility**
+
+This tool is designed to allow the quick and automated transfer of assets from one wallet to another.
+
+This will NOT transfer or remove empty assets by design as these assets should be reviewed to make sure you are not staking them somewhere and to prevent the accidental removal of a needed asset for retrieval.
+
+If after using this you need to remove empty assets, use the Bulk ASA Opt-Out tool created by Kinn: https://www.kinn.info/out
--- main.py
+++ main.py
@@ -12,9 +12,13 @@ algod_client = algod.AlgodClient(algod_token, algod_address, headers)
 
 # Set up sender and recipient information
 sender_address = "Sender Wallet Address here"
-sender_mnemonic = "Sender Private Mnemonic Here"
+sender_mnemonic_input = "Sender Private Mnemonic Here"
 recipient_address = "Recipient Wallet Address here"
-recipient_mnemonic = "Recipient Private Mnemonic here"
+recipient_mnemonic_input = "Recipient Private Mnemonic here"
+
+# Convert Mnemonic to Private Key
+sender_mnemonic = re.sub(",", "", sender_mnemonic_input)
+recipient_mnemonic = re.sub(",", "", recipient_mnemonic_input)
 
 # Convert Mnemonic to Private Key
 sender_private_key = mnemonic.to_private_key(sender_mnemonic)
@@ -23,6 +27,7 @@ recipient_private_key = mnemonic.to_private_key(recipient_mnemonic)
 # Get sender and recipient account info
 sender_account_info = algod_client.account_info(sender_address)
 recipient_account_info = algod_client.account_info(recipient_address)
+
 
 def opt_in_asset(asset_id, account_info, private_key):
     """Opt-in an account to an asset if not already opted-in"""
@@ -37,6 +42,7 @@ def opt_in_asset(asset_id, account_info, private_key):
         signed_optin_txn = optin_txn.sign(private_key)
         algod_client.send_transaction(signed_optin_txn)
         print(f"Recipient opted-in to asset ID {asset_id}")
+
 
 def send_asset(asset_id, asset_amount, sender_address, recipient_address, sender_private_key):
     """Send an asset from a sender to a recipient, closing out the sender's position"""
@@ -53,25 +59,31 @@ def send_asset(asset_id, asset_amount, sender_address, recipient_address, sender
     txid = algod_client.send_transaction(signed_send_txn)
     print(f"Transaction ID: {txid}")
 
-def close_out_asset(asset_id, asset_amount, sender_address, sender_private_key):
+
+def close_out_asset(asset_id, sender_address, recipient_address, sender_private_key):
     """Close out an asset position for a sender"""
     params = algod_client.suggested_params()
+    asset_info = algod_client.asset_info(asset_id)
+    creator_address = asset_info["params"]["creator"]
     send_txn = algosdk.transaction.AssetTransferTxn(
         sender=sender_address,
         sp=params,
-        receiver=sender_address,
+        receiver=creator_address,
         amt=0,
         index=asset_id,
-        close_assets_to=sender_address
+        close_assets_to=creator_address
     )
     signed_send_txn = send_txn.sign(sender_private_key)
     txid = algod_client.send_transaction(signed_send_txn)
     print(f"Transaction ID: {txid}")
 
+
 # Loop through all assets in the sender's account and send them to the recipient
 for asset in sender_account_info["assets"]:
-    asset_id = asset["asset-id"]
     asset_amount = asset["amount"]
+    asset_id = asset["asset-id"]
+    print(f"Asset ID: {asset_id}")
+    print(f"Asset Amount: {asset_amount}")
 
     if asset_amount > 0:
         # Opt-in recipient if necessary
@@ -80,6 +92,6 @@ for asset in sender_account_info["assets"]:
         # Send the asset to the recipient
         send_asset(asset_id, asset_amount, sender_address, recipient_address, sender_private_key)
 
-    else:
-        # Close out the asset position for the sender
-        close_out_asset(asset_id, asset_amount, sender_address, sender_private_key)
+    # else:
+    #     # Close out the asset position for the sender
+    #     close_out_asset(asset_id, sender_address, recipient_address, sender_private_key)
